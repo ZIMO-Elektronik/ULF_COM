2638ccdb9adf20ce3c44d7752bdb8582a10df307
Vincent Hamp <vincent.hamp@higaski.at>
2023-11-17 15:47:09 +0100

    Split example

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,6 @@
 #include <ulf/com.hpp>
 
-int main() {
+void str2mx1bin() {
   // Check if character stream contains valid MX1 binary
   auto maybe_mx1bin{ulf::com::str2mx1bin("\x01\x01"
                                          "data\x17")};
@@ -17,8 +17,15 @@ int main() {
   }
   // Error, not MX1 binary scheme
   else {}
+}
 
+void ping() {
   // Replies to "PING\r"
   auto ping{ulf::com::ping("MyDevice", "1.2.3")};
   auto ping_with_rev{ulf::com::ping("MyDevice", "1.2.3", 'B')};
+}
+
+int main() {
+  str2mx1bin();
+  ping();
 }
